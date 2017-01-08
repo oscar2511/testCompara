98ec3fcb0515a94f0f3e5eeca2369e84485bd5c9
ubibaldo <canrodriguez@gmail.com>
2017-01-08 16:05:10 -0300

    Especificar parametros de funciones para mejorar la comprensión de las mismas

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var Login  = require('./Lib/Login.js');
+
+
 let registeredUsers = {
   user1: 'pass1',
   user2: 'pass2',
@@ -8,9 +11,22 @@ let registeredUsers = {
 
 let login = new Login(registeredUsers);
 
-login.registerUser('user4', 'pass4');
-login.login('user4', 'pass4');
-login.updatePassword('user3', 'pass3', 'pass5');
-login.login('user3', 'pass5');
-login.logout('user4');
-login.logout('user3');
+console.log('Initiating process...')
+
+setTimeout(function () {
+  login.registerUser('user4', 'pass4');
+  login.login('user4', 'pass4');
+  login.updatePassword('user3', 'pass3', 'pass5');
+  login.login('user3', 'pass5');
+}, 2000);
+
+setTimeout(function () {
+  console.log('Actives sessions:')
+  console.log(login.sessions);
+}, 4000)
+
+setTimeout(function () {
+  login.logout('user4');
+  login.logout('user3');
+  console.log('Finished process!');
+}, 6000)
